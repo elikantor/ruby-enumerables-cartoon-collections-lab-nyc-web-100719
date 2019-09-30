46c58413afe2c9b1543e9faf33eb21186214e497
Eli Kantor <eli.kantor@outlook.com>
2019-09-30 17:54:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(array)
-  array.each_with_index do |ele,idx|
-    puts "#{idx} #{ele}"
+  array.each_with_index do |ele, idx|
+    n = 1 + idx
+    puts "#{n}. #{ele}"
   end
   
 end
