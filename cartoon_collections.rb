b71150f1b0bb7168140774ac09f7f663a8bce66f
Eli Kantor <eli.kantor@outlook.com>
2019-09-30 17:56:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,17 +3,20 @@ def roll_call_dwarves(array)
     n = 1 + idx
     puts "#{n}. #{ele}"
   end
+end
+
+def summon_captain_planet(planeteer_calls)
+  newarry = []
   
+  planeteer_calls.map do |ele|
+    newarr << "ele!"
+  end
+  
+  print newarr
 end
 
-
-
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
+  
 end
 
 def find_the_cheese# code an argument here
