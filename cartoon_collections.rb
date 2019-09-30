f3404fc9934699fad521bdea4945e0807b9b6230
Eli Kantor <eli.kantor@outlook.com>
2019-09-30 17:38:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  do array.each_with_index |idx, ele|
+    puts idx ele
+  end
+  
 end
+
+
 
 def summon_captain_planet# code an argument here
   # Your code here
